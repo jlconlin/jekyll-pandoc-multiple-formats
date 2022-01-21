fe7f59ca36fb90215c7d27246bfafa21b80193ff
fauno <fauno@endefensadelsl.org>
2022-01-21 15:57:02 -0300

    test conversion

--- test/jekyll/converters/pandoc_test.rb
+++ test/jekyll/converters/pandoc_test.rb
@@ -16,5 +16,16 @@ class Jekyll::Converters::PandocTest < MiniTest::Test
       assert_includes converter.third_party_processors, :Pandoc
       assert_instance_of Jekyll::Converters::Markdown::Pandoc, converter.instance_variable_get(:@parser)
     end
+
+    should 'convert markdown using pandoc' do
+      site = fixture_site 'markdown' => 'Pandoc'
+
+      converter = site.converters.find do |c|
+        c.class == Jekyll::Converters::Markdown
+      end
+
+      assert converter.setup
+      assert_equal "<h1 id=\"hello\">Hello</h1>\n", converter.convert('# Hello')
+    end
   end
 end
