28fe7bab756110c3a3d3c0b76f52212d1e20983e
fauno <fauno@endefensadelsl.org>
2022-01-21 16:02:10 -0300

    deprecate old converter

--- lib/jekyll-pandoc-multiple-formats.rb
+++ lib/jekyll-pandoc-multiple-formats.rb
@@ -1,29 +1,7 @@
-# Copyright (c) 2012-2015 Nicolás Reynolds <fauno@endefensadelsl.org>
-#               2012-2013 Mauricio Pasquier Juan <mpj@endefensadelsl.org>
-#               2013      Brian Candler <b.candler@pobox.com>
-# 
-# Permission is hereby granted, free of charge, to any person obtaining
-# a copy of this software and associated documentation files (the
-# "Software"), to deal in the Software without restriction, including
-# without limitation the rights to use, copy, modify, merge, publish,
-# distribute, sublicense, and/or sell copies of the Software, and to
-# permit persons to whom the Software is furnished to do so, subject to
-# the following conditions:
-# 
-# The above copyright notice and this permission notice shall be
-# included in all copies or substantial portions of the Software.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# frozen_string_literal: true
 
-require 'open3'
+require_relative 'jekyll/converters/pandoc'
 
-require 'jekyll-pandoc-multiple-formats/version'
 require 'jekyll-pandoc-multiple-formats/config'
 
 # TODO this may go to a separate gem
@@ -34,4 +12,3 @@ require 'jekyll-pandoc-multiple-formats/unite'
 
 require 'jekyll-pandoc-multiple-formats/pandoc_file'
 require 'jekyll-pandoc-multiple-formats/generator'
-require 'jekyll-pandoc-multiple-formats/converter'
